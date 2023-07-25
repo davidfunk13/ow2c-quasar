de4df2c23586c100bc27236e40105227ec9398b1
Dave Funk <davefunk135@gmail.com>
2023-07-24 20:49:07 -0600

    quasar proj init

---
+++ src/components/LeftDrawer/LeftDrawer.vue
@@ -0,0 +1,13 @@
+<template>
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+        <!-- drawer content -->
+    </q-drawer>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    name: 'EssentialLink',
+});
+</script>
